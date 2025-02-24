7f00b5d1ae150a59e1ed71f6ef254db7039e8a4a
iloveudra <breno1thb@gmail.com>
2025-02-23 22:32:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function selectOption(option) {
             displayCatHeart(); // Display the cat-heart.gif
         });
     } else if (option === 'no') {
-        // Change text on the "No" button to "You sure?"
+        // Change text on the "No" button to "Tem certeza?"
         document.getElementById('no-button').innerText = 'You sure?'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
